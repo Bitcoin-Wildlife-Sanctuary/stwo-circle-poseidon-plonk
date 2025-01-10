e2daeee3dfcf33f59e706bf0c9a52210db01aff4
weikengchen <w.k@berkeley.edu>
2025-01-10 19:10:04 +0800

    progress

--- crates/prover/src/examples/plonk_with_poseidon/poseidon/mod.rs
+++ crates/prover/src/examples/plonk_with_poseidon/poseidon/mod.rs
@@ -1,5 +1,15 @@
 use std::ops::{Add, AddAssign, Mul, Sub};
-use crate::core::fields::m31::BaseField;
+use num_traits::One;
+use crate::constraint_framework::{EvalAtRow, RelationEntry, PREPROCESSED_TRACE_IDX};
+use crate::core::fields::FieldExpOps;
+use crate::core::fields::m31::{BaseField, M31};
+use crate::core::vcs::poseidon31_ref::{FIRST_FOUR_ROUND_RC, LAST_FOUR_ROUNDS_RC, PARTIAL_ROUNDS_RC};
+use crate::examples::poseidon::PoseidonElements;
+use crate::examples::xor::gkr_lookups::accumulation::DynMle::Base;
+
+const N_STATE: usize = 16;
+const N_HALF_FULL_ROUNDS: usize = 4;
+const N_PARTIAL_ROUNDS: usize = 14;
 
 #[inline(always)]
 /// Applies the M4 MDS matrix described in <https://eprint.iacr.org/2023/323.pdf> 5.1.
@@ -47,4 +57,144 @@ where
             state[4 * i + j] += s.clone();
         }
     }
+}
+
+
+// Applies the internal round matrix.
+//   mu_i = 2^{i+1} + 1.
+// See <https://eprint.iacr.org/2023/323.pdf> 5.2.
+fn apply_internal_round_matrix<F>(state: &mut [F; 16])
+where
+    F: Clone + AddAssign<F> + Add<F, Output = F> + Sub<F, Output = F> + Mul<BaseField, Output = F>,
+{
+    let sum = state[1..]
+        .iter()
+        .cloned()
+        .fold(state[0].clone(), |acc, s| acc + s);
+
+    state[0] += state[0].clone() + state[0].clone() + sum.clone();
+    state.iter_mut().enumerate().skip(1).for_each(|(i, s)| {
+        // TODO(andrew): Change to rotations.
+        *s = s.clone() * BaseField::from_u32_unchecked(1 << (i + 1)) + sum.clone();
+    });
+}
+
+fn pow5<F: FieldExpOps>(x: F) -> F {
+    let x2 = x.clone() * x.clone();
+    let x4 = x2.clone() * x2.clone();
+    x4 * x.clone()
+}
+
+pub fn eval_poseidon_constraints<E: EvalAtRow>(eval: &mut E, lookup_elements: &PoseidonElements) {
+    let addr_1 = eval.next_interaction_mask(PREPROCESSED_TRACE_IDX, [0]);
+    let addr_2 = eval.next_interaction_mask(PREPROCESSED_TRACE_IDX, [0]);
+    let addr_3 = eval.next_interaction_mask(PREPROCESSED_TRACE_IDX, [0]);
+    let addr_4 = eval.next_interaction_mask(PREPROCESSED_TRACE_IDX, [0]);
+
+    let sel_1 = eval.next_trace_mask();
+    let sel_2 = eval.next_trace_mask();
+    let sel_3 = eval.next_trace_mask();
+    let sel_4 = eval.next_trace_mask();
+
+    let mut state: [_; N_STATE] = std::array::from_fn(|_| eval.next_trace_mask());
+
+    // Require state lookup.
+    let initial_state = state.clone();
+
+    // 4 full rounds.
+    (0..N_HALF_FULL_ROUNDS).for_each(|round| {
+        (0..N_STATE).for_each(|i| {
+            state[i] += FIRST_FOUR_ROUND_RC[round][i];
+        });
+        state = std::array::from_fn(|i| pow5(state[i].clone()));
+        apply_external_round_matrix(&mut state);
+        state.iter_mut().for_each(|s| {
+            let m = eval.next_trace_mask();
+            eval.add_constraint(s.clone() - m.clone());
+            *s = m;
+        });
+    });
+
+    // Partial rounds.
+    (0..N_PARTIAL_ROUNDS).for_each(|round| {
+        state[0] += PARTIAL_ROUNDS_RC[round];
+        state[0] = pow5(state[0].clone());
+        apply_internal_round_matrix(&mut state);
+        let m = eval.next_trace_mask();
+        eval.add_constraint(state[0].clone() - m.clone());
+        state[0] = m;
+    });
+
+    // 4 full rounds.
+    (0..N_HALF_FULL_ROUNDS).for_each(|round| {
+        (0..N_STATE).for_each(|i| {
+            state[i] += LAST_FOUR_ROUNDS_RC[round][i];
+        });
+        state = std::array::from_fn(|i| pow5(state[i].clone()));
+        apply_external_round_matrix(&mut state);
+        state.iter_mut().for_each(|s| {
+            let m = eval.next_trace_mask();
+            eval.add_constraint(s.clone() - m.clone());
+            *s = m;
+        });
+    });
+
+    eval.add_to_relation(RelationEntry::new(
+        lookup_elements,
+        E::EF::one(),
+        &[addr_1, sel_1.clone()]
+    ));
+
+    eval.add_to_relation(RelationEntry::new(
+        lookup_elements,
+        E::EF::one(),
+        &[addr_2, sel_2.clone()]
+    ));
+
+    eval.add_to_relation(RelationEntry::new(
+        lookup_elements,
+        E::EF::one(),
+        &[addr_3, sel_3.clone()]
+    ));
+
+    eval.add_to_relation(RelationEntry::new(
+        lookup_elements,
+        E::EF::one(),
+        &[addr_4, sel_4.clone()]
+    ));
+
+    for i in 0..8 {
+        eval.add_to_relation(RelationEntry::new(
+            lookup_elements,
+            E::EF::one(),
+            &[sel_1.clone() + BaseField::from(i), initial_state[i].clone()]
+        ));
+    }
+
+    for i in 0..8 {
+        eval.add_to_relation(RelationEntry::new(
+            lookup_elements,
+            E::EF::one(),
+            &[sel_2.clone() + BaseField::from(i), initial_state[i + 8].clone()]
+        ))
+    }
+
+    for i in 0..8 {
+        eval.add_to_relation(RelationEntry::new(
+            lookup_elements,
+            E::EF::one(),
+            &[sel_3.clone() + BaseField::from(i), state[i].clone()]
+        ))
+    }
+
+    for i in 0..8 {
+        eval.add_to_relation(RelationEntry::new(
+            lookup_elements,
+            E::EF::one(),
+            &[sel_4.clone() + BaseField::from(i), state[i + 8].clone()]
+        ))
+    }
+
+    // TODO: use higher degrees batching
+    eval.finalize_logup_in_pairs();
 }

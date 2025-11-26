f62d0311b13c8944bd7f810b22b1d039bab4b540
weikengchen <w.k@berkeley.edu>
2025-11-27 01:17:50 +0800

    update the assert for proof generation

--- crates/examples/src/plonk_without_poseidon/air.rs
+++ crates/examples/src/plonk_without_poseidon/air.rs
@@ -154,7 +154,7 @@ where
         commitment_scheme
             .polynomials()
             .as_cols_ref()
-            .map_cols(|c| c.evals.domain.log_size())
+            .map_cols(|c| c.evals.domain.log_size() - config.fri_config.log_blowup_factor)
             .0,
         stmt0.log_sizes().0
     );
